8b11223d66a24ee3f67ccbc24661ffc3fc12250e
Hossam Ezzat <77586282+HossamEzzat@users.noreply.github.com>
2022-07-10 15:08:00 +0200

    Add files via upload

---
+++ N. Numbers Histogram/main.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	char symbol;
+	cin>>symbol;
+	int lines;
+	cin>>lines;
+	for(int i=1;i<=lines;i++){
+		int times;
+		cin>>times;
+		while(times--){
+			cout<<symbol;
+		}
+		cout<<endl;
+	}
+}
---
+++ O. Pyramid/main.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int counter;
+	cin>>counter;
+	for(int i=1;i<=counter;i++){
+		for(int j=1;j<=i;j++){
+			cout<<"*";
+		}
+		cout<<endl;
+	}
+}
