a3ef074ef6f66b0d854a23cd242dffc581d8a788
Hossam Ezzat <77586282+HossamEzzat@users.noreply.github.com>
2022-07-28 11:23:58 +0200

    Add files via upload

---
+++ D. Positions in array/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int size;
+	cin>>size;
+	int arr[size];
+	for(int i=0;i<size;i++){
+        cin>>arr[i];
+		if(arr[i] <= 10){
+			cout<<"A["<<i<<"] = "<<arr[i]<<endl;
+		}
+	}
+}
