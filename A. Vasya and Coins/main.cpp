b967806e52810dcec8914731432436ae944d75e5
Hossam Ezzat <77586282+HossamEzzat@users.noreply.github.com>
2022-08-25 11:34:05 +0200

    Add files via upload

---
+++ A. Vasya and Coins/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--){
+
+    int a,b;
+    cin>>a>>b;
+    if(a==0){
+        cout<<1<<endl;
+    }else{
+
+     cout<<2*b+a+1<<endl;
+
+
+    }
+
+
+    }
+    return 0;
+}
