57d5ccbb8e70916eb295faacdc669d58e11a47d5
Hossam Ezzat <77586282+HossamEzzat@users.noreply.github.com>
2022-11-02 13:12:22 +0200

    Add files via upload

---
+++ A. 2-3 Moves/main.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+#define ll long long
+
+
+int main()
+{
+    int t;
+    cin>>t;
+
+    while(t--) {
+        int n;
+        cin>>n;
+
+        if(n == 1) {
+            cout<<"2"<<endl;
+            continue;
+        }
+
+        if(n == 2) {
+            cout<<"1"<<endl;
+            continue;
+        }
+
+        if(n % 3 == 0) {
+            cout<< n / 3<<endl;
+            continue;
+        }
+
+        cout<<n / 3 + 1<<endl;
+
+
+    }
+
+    return 0;
+}
